c72025260fa27f46d58cc3c417a272894e250201
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2022-01-12 01:32:50 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+            string saizeName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontSize = Convert.ToDouble(saizeName);
+            }
+
+        }
+        bool Bold;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Bold == false)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+            Bold = !Bold;
+        }
+        bool Italic;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Italic == false)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+            Italic = !Italic;
+        }
+        bool Underline;
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Underline == false)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Baseline;
+            }
+            Underline = !Underline;
+
+
+
+        }
+        bool check;
+        private void RadioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+
+                textBox.Foreground = Brushes.Black;
+
+
+        }
+        bool check1;
+        private void RadioButton2_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.Foreground = Brushes.Red;
+        }
+    }
+}
